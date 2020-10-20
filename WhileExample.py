1cd40146f1b9fcca5b51d9fc6bbae17f46371695
ShabsSultana <shabnur_sultana@hotmail.com>
2020-10-20 14:59:40 +0100

    While Example

---
+++ WhileExample.py
@@ -0,0 +1,9 @@
+#programme prints yes until the user input is not yes
+
+condition = "yes"
+
+while condition == "yes":
+    print("Hello")
+    condition = input("Would you like to run the program again? ")
+
+print("Goodbye")
